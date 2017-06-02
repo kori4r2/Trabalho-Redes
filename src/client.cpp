36fbcd947d0ae96429ff9e32627203dc253a59c9
Italo Tobler <xitsilvax@hotmail.com>
2017-06-02 10:07:38 -0300

    commented templates

--- src/client.cpp
+++ src/client.cpp
@@ -1,21 +1,19 @@
 #include "Pressure.hpp"
 #include<stdio.h>
 
-// compile using : g++ main.cpp -o ./src/Sensor.o ./src/Pressure.o -o main
-
 using namespace std;
 
 int main(){
-	Pressure * sensor =NULL;
-	char c ='1';
-	double dtime = 10;
 
-	sensor = new Pressure(&dtime);
-	while(c!='d'){
-		printf("Pressure: %lf\n", sensor->getMeasure());
-		scanf("%c", &c);
-	}
-	delete(sensor);
+	// Criar todos os sensores
+	// Criar todos os sockets (um para cada sensor)
+	// Conectar cada socket ao servidor na ordem esperada pelo servidor
+	// Utiliza um for para enviar um numero maximo de mensagens
+	// 	Recebe uma mensagem do server usando a funcao read (sai do loop caso nao consiga conectar)
+	// 	Envia os novos valores dos sensores utilizando a funcao write e colocando cada chamada em uma thread
+	// 	Da join em todas as threads criadas
+	// Desconecta caso ainda esteja conectado
+	// Apaga toda a memoria alocada
 	
 	return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,23 +1,18 @@
 #include "Pressure.hpp"
 #include<stdio.h>
 
-// compile using : g++ main.cpp -o ./src/Sensor.o ./src/Pressure.o -o main
-
 using namespace std;
 
 int main2(){
-	Pressure * sensor =NULL;
-	char c ='1';
-	double dtime = 10;
-
-	sensor = new Pressure(&dtime);
-
-	while(c!='d'){
-		printf("Pressure: %lf\n", sensor->getMeasure());
-		scanf("%c", &c);
-	}
-	delete(sensor);
-
+	// Criar lista de sockets em ordem pre-definida (1 socket para cada sensor)
+	// Conectar todos os sockets, em ordem
+	// Enquanto houver ao menos um socket conectado
+	// 	Enviar mensagem de inicio
+	// 	Iniciar um read em cada socket, com cada chamada em uma thread
+	// 	join nas threads criadas
+	// 	Calcular sensores virtuais de acordo com os alores recebidos
+	// 	Exibir as informacoes na tela
+	// Apaga toda a memoria alocada
 	return 0;
 
 }
